7075d86d49998d6c5a54afe77e80ac5b6447700a
Hiếu Trần Minh <hieutranminh1998@gmail.com>
2020-01-06 13:42:15 +0700

    abc

--- src/main/java/com/cicd/CicdAppliedToSpringBootJavaAppApplication.java
+++ src/main/java/com/cicd/CicdAppliedToSpringBootJavaAppApplication.java
@@ -6,13 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class CicdAppliedToSpringBootJavaAppApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "hello";
-	}
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CicdAppliedToSpringBootJavaAppApplication.class, args);
